e27d4a8340badfed0ace325cce6056c701035eac
Thiago Henrique Felix Cândido Ribeiro Conceição <ricandido5@gmail.com>
2023-11-23 01:10:48 -0300

    Servidor FTP

--- provisioners/ftp_provision.sh
+++ provisioners/ftp_provision.sh
@@ -4,18 +4,12 @@ set -e
 
 apt update
 apt install -y docker.io
-sudo docker pull ustclug/ftp
+docker pull bogem/ftp
 
-LOCAL_DIR=$(pwd)
-sudo docker run -itd --restart=always \
-    -p 20:20 \
-    -p 21:21 \
-    -p 2222:22 \
-    -p 8080:80 \
-    -p 40000-50000:40000-50000 \
-    -p 990:990 \
-    -v $LOCAL_DIR/data:/srv/ftp \
-    -v $LOCAL_DIR/log:/var/log \
-    -e PRIVATE_PASSWD=secret \
-    -e PASV_ADDRESS=192.168.56.10 \
-    ustclug/ftp
+sudo docker run -d -v /vagrantFTP:/home/vsftpd \
+	-p 20:20 -p 21:21 -p 47400-47470:47400-47470 \
+	-e FTP_USER=teste \
+	-e FTP_PASS=teste123 \
+	-e PASV_ADDRESS=172.17.0.1 \
+	--name ftp \
+	--restart=always bogem/ftp
