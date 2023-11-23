835dfd765cdb88c85833c5431cafe7b36ea1c5b4
Orlando Filho <orlandossf04@gmail.com>
2023-11-23 03:26:52 -0300

    update nfs

--- provisioners/nfs_provision.sh
+++ provisioners/nfs_provision.sh
@@ -4,6 +4,6 @@ apt update
 sudo apt install -y docker.io
 docker pull itsthenetwork/nfs-server-alpine
 
-sudo docker run -d --restart always --name nfs -p 2049:2049 --privileged -v /some/where/fileshare:/nfsshare -e SHARED_DIRECTORY=/nfsshare itsthenetwork/nfs-server-alpine:latest
+sudo docker run -d --restart always --name nfs -p 2049:2049 --privileged -v /DockerNFS/:/nfsshare/ -e SHARED_DIRECTORY=/nfsshare/ itsthenetwork/nfs-server-alpine:latest
 
 
