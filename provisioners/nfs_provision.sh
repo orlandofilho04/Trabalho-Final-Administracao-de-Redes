0cc2b0c864245a0bfe5ac40f4c7789c921159633
Thiago Henrique Felix Cândido Ribeiro Conceição <ricandido5@gmail.com>
2023-11-24 14:23:24 -0300

    teste

--- provisioners/nfs_provision.sh
+++ provisioners/nfs_provision.sh
@@ -4,6 +4,6 @@ apt update
 sudo apt install -y docker.io
 docker pull thiagofelix/nfs_ubuntu
 
-sudo docker run -d --restart always -e SYNC=true --name nfs -p 2049:2049 --privileged -v /DockerNFS/:/nfsshare -e SHARED_DIRECTORY=/nfsshare thiagofelix/nfs_ubuntu
+sudo docker run -d --restart always -e SYNC=true --name nfs -p 2049:2049 --privileged -v /vagrantNFS:/nfsshare -e SHARED_DIRECTORY=/nfsshare thiagofelix/nfs_ubuntu
 
 
